06d61723feb60fb91a5409420b94ecad355106b3
asmitbhantana <bhantanasmit@gmail.com>
2020-06-29 13:55:25 +0545

    Fixed some errors.

--- PythonProgrammingPractise/Functions/f14.py
+++ PythonProgrammingPractise/Functions/f14.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     user_list_with_dict = [{'make': 'Nokia', 'model': 'Nokia Keypad', 'color': 'Black'},
                            {'make': 'Mi A1', 'model': 'tissot', 'color': 'Gold'},
                            {'make': 'Samsung Fold', 'model': 'flexi', 'color': 'Blue'}]
-    sort_dictionary_list_using_lambda(user_list_with_dict)
-    print(user_list_with_dict)
+    print(sort_dictionary_list_using_lambda(user_list_with_dict)
+)
