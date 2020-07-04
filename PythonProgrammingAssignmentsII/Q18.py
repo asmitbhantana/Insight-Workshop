a6f613ada66ccf2c40f68e38d02381b66291a2ee
asmitbhantana <bhantanasmit@gmail.com>
2020-07-04 11:43:08 +0545

    Completed the assignments.

--- PythonProgrammingAssignmentsII/Q18.py
+++ PythonProgrammingAssignmentsII/Q18.py
@@ -7,11 +7,38 @@ to your age, to a JSON string. Deserialize the JSON back into
 Python.
 """
 
-# import json
-#
-# user_info = {
-#     'name': 'Asmit Bhantana',
-#     'age': 10
-# }
-# loading_json = json.load(user_info)
-# print(dir(loading_json))
+import json
+
+usr_dict = {"name": "Asmit Bhantana", "age": "22"}
+
+
+# encoding json
+
+# custom way
+class NameAndAgeEncoder(json.JSONEncoder):
+    def default(self, my_dict):
+        return f"'name':'{my_dict['name']},'age':'{my_dict['age']}'"
+
+
+usr_encoded_string = json.dumps(usr_dict, cls=NameAndAgeEncoder)
+print("Encoding user string my way\n",usr_encoded_string)
+
+# default way
+print("Encoding user string default way\n",json.dumps(usr_dict))
+
+# decoding json
+
+# default way
+my_decoder = json.JSONDecoder()
+usr_decoded_string = my_decoder.decode(s=usr_encoded_string)
+print("Decoding my string default way\n",usr_decoded_string)
+
+
+# custom way
+def decode_user_name_and_age(usr_encoded_string):
+     usr_encoded_string['__custom__'] = True
+     return usr_encoded_string
+
+
+usr_decoded_string = json.loads(usr_encoded_string, object_hook=decode_user_name_and_age)
+print("Decoding my string custom way\n",usr_decoded_string)
--- PythonProgrammingAssignmentsII/Q20.py
+++ PythonProgrammingAssignmentsII/Q20.py
@@ -4,3 +4,23 @@ from a list of n real numbers.
 Input array : [-25, -10, -7, -3, 2, 4, 8, 10]
 Output : [[-10, 2, 8], [-7, -3, 10]]
 """
+if __name__ == '__main__':
+    usr_list = [-25, -10, -7, -3, 2, 4, 8, 10]
+    required_result = 0
+    usr_list.sort()
+    required_result_num_list = []
+    for i in range(len(usr_list)):
+        if usr_list[i] >= required_result:
+            break
+        for j in range(i + 1, len(usr_list)):
+            if usr_list[i]+usr_list[j] >= required_result:
+                break
+            for k in range(j + 1, len(usr_list)):
+                c_sum = usr_list[i] + usr_list[j] + usr_list[k]
+                if c_sum > required_result:
+                    break
+                elif c_sum == required_result:
+                    required_result_num_list.append([usr_list[i], usr_list[j], usr_list[k]])
+                    break
+
+    print(required_result_num_list)
